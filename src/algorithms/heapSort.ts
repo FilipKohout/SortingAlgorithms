52a361e8af775ef6625c7cef8585bc1b56812566
FilipKohout <filipekkohout@gmail.com>
2026-01-06 11:58:41 +0100

    add heap sort

---
+++ src/algorithms/heapSort.ts
@@ -0,0 +1,37 @@
+export default function heapSort(data: string, onProgress: (p: number) => void) {
+    const arr = data.split("\n").map(Number);
+
+    function heapify(n: number, i: number) {
+        let largest = i;
+        const left = 2 * i + 1;
+        const right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest !== i) {
+            [arr[i], arr[largest]] = [arr[largest], arr[i]];
+
+            heapify(n, largest);
+        }
+    }
+
+    const n = arr.length;
+
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--)
+        heapify(n, i);
+
+    for (let i = n - 1; i > 0; i--) {
+        [arr[0], arr[i]] = [arr[i], arr[0]];
+
+        onProgress(((n - i) / n) * 100);
+        heapify(i, 0);
+    }
+
+    onProgress(100);
+
+    return arr;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import algorithmWrapper from "./algorithmWrapper";
 import selectionSort from "./algorithms/selectionSort";
+import heapSort from "./algorithms/heapSort";
 
-await algorithmWrapper(selectionSort, "./raw/random_words_10M.txt");
+await algorithmWrapper(heapSort, "./raw/random_words_1M.txt");
+await new Promise(resolve => setTimeout(resolve, 100000000));
