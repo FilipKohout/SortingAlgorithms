fa7f08f141c159fdeb49366b48e7225f65961041
FilipKohout <filipekkohout@gmail.com>
2026-01-06 11:58:27 +0100

    add merge sort

---
+++ src/algorithms/mergeSort.ts
@@ -0,0 +1,57 @@
+export default function mergeSort(data: string, onProgress: (p: number) => void) {
+    const arr = data.split("\n").map(Number);
+
+    function merge(left: number, mid: number, right: number) {
+        const lengthLeft = mid - left + 1;
+        const lengthRight = right - mid;
+
+        const leftArr = new Array(lengthLeft);
+        const rightArr = new Array(lengthRight);
+
+        for (let i = 0; i < lengthLeft; i++)
+            leftArr[i] = arr[left + i];
+
+        for (let j = 0; j < lengthRight; j++)
+            rightArr[j] = arr[mid + 1 + j];
+
+        let iLeft = 0, iRight = 0, iMerged = left;
+
+        while (iLeft < lengthLeft && iRight < lengthRight) {
+            if (leftArr[iLeft] <= rightArr[iRight]) {
+                arr[iMerged] = leftArr[iLeft];
+                iLeft++;
+            } else {
+                arr[iMerged] = rightArr[iRight];
+                iRight++;
+            }
+
+            iMerged++;
+        }
+
+        while (iLeft < lengthLeft) {
+            arr[iMerged] = leftArr[iLeft];
+
+            iLeft++;
+            iMerged++;
+        }
+
+        while (iRight < lengthRight) {
+            arr[iMerged] = rightArr[iRight];
+
+            iRight++;
+            iMerged++;
+        }
+    }
+
+    function sort(left: number, right: number) {
+        if (left === right) return;
+
+        const mid = Math.floor((left + right) / 2);
+
+        sort(left, mid);
+        sort(mid + 1, right);
+        merge(left, mid, right);
+    }
+
+    sort(0, arr.length - 1);
+}
